af8b0432245dbf1d146b43c1ea412a85e7366508
nguyenit67 <iamnghoangnguyen@gmail.com>
2022-02-01 00:48:12 +0700

    Fix `less` bug import `~antd/dist/antd.less`

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import vitePluginImp from 'vite-plugin-imp';
+
 // import reactRefresh from "@vitejs/plugin-react-refresh";
 
 // https://vitejs.dev/config/
@@ -8,5 +10,23 @@ export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
+    vitePluginImp({
+      libList: [
+        {
+          libName: 'antd',
+          style: (name) => `antd/es/${name}/style`,
+        },
+      ],
+    }),
   ],
+  css: {
+    preprocessorOptions: {
+      less: {
+        javascriptEnabled: true,
+      },
+    },
+  },
+  resolve: {
+    alias: [{ find: /^~/, replacement: '' }],
+  },
 });
